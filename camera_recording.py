0539265b8764b6b4be956f5662242cdfc94df48d
Ashu Kumar <30663362+kin-kins@users.noreply.github.com>
2018-06-30 03:12:58 +0530

    Update and rename camera_recording to camera_recording.py

--- camera_recording.py
+++ camera_recording.py
@@ -1,5 +1,12 @@
+from picamera import PiCamera
+from time import sleep
+
+camera=PiCamera()
+
 camera.start_preview()
 camera.start_recording('/home/pi/video.h264')
+
 sleep(10)
+
 camera.stop_recording()
 camera.stop_preview()
